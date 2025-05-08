d72064ffb24a327713fa33356b3e727520969fc9
7shi <7shi@live.jp>
2025-05-09 04:02:06 +0900

    feat: REPLモードをサポート

--- vizprompt/cli/commands.py
+++ vizprompt/cli/commands.py
@@ -1,14 +1,12 @@
 '''VizPromptのコマンドラインインターフェース'''
 import argparse
-
-from ..llm import gemini
-from ..llm import ollama
 from ..core.node import NodeSaver
 
-def handle_chat(prompt, response, model):
+def handle_chat(generate_content, prompt):
+    response, model = generate_content(prompt)
     prompt = prompt.rstrip()
     response = response.rstrip()
-    print(f"ユーザー: {prompt}")
+    print(f"User: {prompt}")
     print(f"{model}: {response}")
 
     # --- ノード保存処理 ---
@@ -26,23 +24,37 @@ def run_cli():
 
     # 'chat gemini' サブコマンド
     gemini_parser = chat_subparsers.add_parser("gemini", help="Geminiとチャットします")
-    gemini_parser.add_argument("prompt", type=str, help="Geminiへのプロンプト")
+    gemini_parser.add_argument("prompt", type=str, nargs="?", help="Geminiへのプロンプト")
 
     # 'chat ollama' サブコマンド
     ollama_parser = chat_subparsers.add_parser("ollama", help="Ollamaとチャットします")
-    ollama_parser.add_argument("prompt", type=str, help="Ollamaへのプロンプト")
+    ollama_parser.add_argument("prompt", type=str, nargs="?", help="Ollamaへのプロンプト")
 
     args = parser.parse_args()
 
     if args.command == "chat":
+        generate_content = None
         if args.service == "gemini":
-            response, model = gemini.generate_content(args.prompt)
-            handle_chat(args.prompt, response, model)
+            from ..llm import gemini
+            generate_content = gemini.generate_content
         elif args.service == "ollama":
-            response, model = ollama.generate_content(args.prompt)
-            handle_chat(args.prompt, response, model)
-        else:
+            from ..llm import ollama
+            generate_content = ollama.generate_content
+        if generate_content is None:
             chat_command_parser.print_help()
+        else:
+            if args.prompt is None:
+                # REPLモード
+                while True:
+                    try:
+                        prompt = input(">>> ").rstrip()
+                        if not prompt:
+                            break
+                        handle_chat(generate_content, prompt)
+                    except EOFError:
+                        break
+            else:
+                handle_chat(generate_content, args.prompt)
     else:
         parser.print_help()
 
